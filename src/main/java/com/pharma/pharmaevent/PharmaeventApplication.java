db94c35144b3c2a099f8f4adae83b9d51c8c7f08
Gautham Dhinakar <gautham.dhinakar@gmail.com>
2026-01-06 16:09:03 +0530

    UI Changes: Background and Menu Modification

--- src/main/java/com/pharma/pharmaevent/PharmaeventApplication.java
+++ src/main/java/com/pharma/pharmaevent/PharmaeventApplication.java
@@ -1,4 +1,4 @@
-package com.pharma.pharmaevent;
+   package com.pharma.pharmaevent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
